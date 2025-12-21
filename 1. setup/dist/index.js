360f251550fe6559cbff3eca8bec59dc21895056
ScriptiveMen <i.satyakumarram@gmail.com>
2025-12-21 22:18:14 +0530

    how typescript setups

---
+++ 1. setup/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Welcome ${name}`;
+}
+const username = "satyakrram";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ 1. setup/src/index.ts
@@ -0,0 +1,6 @@
+function greet(name: string): string {
+    return `Welcome ${name}`;
+}
+
+const username: string = "satyakrram";
+console.log(greet(username));
